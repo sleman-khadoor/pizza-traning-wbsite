9386f7169d7ae12b69a36770651ab79a8faed83a
slemankhadoorit <slemankhadoorit@gmail.com>
2021-01-17 07:20:03 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+ <?php include("templates/header.php") ?>
+ <?php include("templates/footer.php") ?>
+
+</body>
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+<footer class="section">
+    <div class="center gray-text">Copyright Sleman Khadoor 2020</div>
+</footer>
+</body>
